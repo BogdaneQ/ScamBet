af5c91284a32df16cef3cf49448c93e3e49da751
Mati <Mati@MATI>
2024-06-01 17:22:29 +0200

    <commit>

--- ScamBet/Controllers/HomeController.cs
+++ ScamBet/Controllers/HomeController.cs
@@ -11,13 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 public class HomeController : Controller
 {
     private readonly BookmacherDBContext _context;
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(BookmacherDBContext context, ILogger<HomeController> logger)
-    {
-        _context = context;
-        _logger = logger;
-    }
 
     public IActionResult Privacy()
     {
@@ -32,34 +25,6 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Login()
     {
-        return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Login(LoginViewModel model)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View(model);
-        }
-
-        // Find the user by username
-        var user = _context.accounts.Include(m => m.role_ID).FirstOrDefault(m => m.username == model.Username);
-        if (user == null)
-        {
-            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-            return View(model);
-        }
-
-        // Validate the password (assuming saltedPassword is hashed with salt)
-        var saltedPassword = HashPasswordWithSalt(model.Password, user.email);
-        if (user.password != saltedPassword)
-        {
-            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-            return View(model);
-        }
-
-        _logger.LogInformation("User logged in.");
         return View();
     }
 
