358d1efe39ef13f78ccde2719a09d892ac2d23be
Mati <Mati@MATI>
2024-06-08 14:35:17 +0200

    <commit>

---
+++ ScamBet/Controllers/LiveGameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScamBet.Controllers
+{
+    public class LiveGameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ScamBet/Models/ChatHub.cs
+++ ScamBet/Models/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿// ChatHub.cs
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace ScamBet.Hubs
